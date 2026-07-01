145b72d4477c93a4bd7c0fd4d7b72d33c35e6f77
Shailee Singh <shailees1102@gmail.com>
2026-07-01 20:58:03 +0530

    Time: 4131 ms (25.44%), Space: 28.8 MB (93.64%) - LeetHub

---
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.py
@@ -0,0 +1,62 @@
+from typing import List
+from collections import deque
+
+class Solution:
+    def maximumSafenessFactor(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+
+        dist = [[-1] * n for _ in range(n)]
+        q = deque()
+
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    dist[i][j] = 0
+                    q.append((i, j))
+
+        dirs = [(1,0), (-1,0), (0,1), (0,-1)]
+
+        while q:
+            x, y = q.popleft()
+            for dx, dy in dirs:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < n and 0 <= ny < n and dist[nx][ny] == -1:
+                    dist[nx][ny] = dist[x][y] + 1
+                    q.append((nx, ny))
+
+        def can(threshold):
+            if dist[0][0] < threshold:
+                return False
+
+            vis = [[False] * n for _ in range(n)]
+            q = deque([(0, 0)])
+            vis[0][0] = True
+
+            while q:
+                x, y = q.popleft()
+
+                if x == n - 1 and y == n - 1:
+                    return True
+
+                for dx, dy in dirs:
+                    nx, ny = x + dx, y + dy
+                    if (0 <= nx < n and 0 <= ny < n and
+                        not vis[nx][ny] and
+                        dist[nx][ny] >= threshold):
+                        vis[nx][ny] = True
+                        q.append((nx, ny))
+
+            return False
+
+        left, right = 0, max(max(row) for row in dist)
+        ans = 0
+
+        while left <= right:
+            mid = (left + right) // 2
+            if can(mid):
+                ans = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return ans
