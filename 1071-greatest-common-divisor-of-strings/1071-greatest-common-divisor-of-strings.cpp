9551f6ea18c5a3630ca255fa10d477a93b2f905c
Shailee Singh <shailees1102@gmail.com>
2026-07-15 12:13:28 +0530

    Time: 0 ms (100%), Space: 9.7 MB (60.81%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        if (str1 + str2 != str2 + str1){
+        if(str1 + str2 != str2 + str1){
             return "";
         }
-        int len = gcd(str1.size(), str2.size());
-        return str1.substr(0,len);
+        return str1.substr(0, gcd(str1.length(), str2.length()));
+        
     }
 };
